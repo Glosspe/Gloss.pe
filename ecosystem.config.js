7700c7dc4eb43ee95c3c405abf50f751754f3832
Sycompro <syscomecosistemadigital@gmail.com>
2026-06-26 03:33:26 -0500

    chore: agregar configuracion PM2 ecosystem para orquestacion local

---
+++ ecosystem.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  apps: [
+    {
+      name: 'tienda-gloss-api',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'dev -p 3001',
+      cwd: './',
+      instances: 1,
+      exec_mode: 'fork',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+      }
+    },
+    {
+      name: 'tienda-gloss-tunnel',
+      script: 'C:\\Users\\Administrador\\Desktop\\dato.click\\ngrok.exe',
+      args: 'http 3001',
+      cwd: './',
+      instances: 1,
+      exec_mode: 'fork',
+      autorestart: true,
+      watch: false,
+    }
+  ]
+};
